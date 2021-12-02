9a51a4537f031f75cdee6f1187fe0217a90a8347
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-02 03:28:20 +0000

    pass socket object to setupInput function to receive user key input and send the server commands to move the snake within the game

--- input.js
+++ input.js
@@ -1,22 +1,49 @@
+
+// stores the active TCP connection object
+let connection;
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  const handleUserInput = (data) => {
-    if (data === '\u0003') {
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
       process.exit();
+    }
+
+    switch (key) {
+      case 'w': {
+        connection.write('Move: up');
+        break;
+      }
+
+      case 'a': {
+        connection.write('Move: left');
+        break;
+      }
+
+      case 's': {
+        connection.write('Move: down');
+        break;
+      }
+
+      case 'd': {
+        connection.write('Move: right');
+        break;
+      }
     }
   };
 
-  stdin.on('data', handleUserInput);
+stdin.on('data', handleUserInput);
 
-  return stdin;
+return stdin;
 };
 
-module.exports = { 
+module.exports = {
   setupInput
 }
