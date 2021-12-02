32c5e9db038c67d3bcc683330d92f044752a0813
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-02 01:44:04 +0000

    made connect fn modular (in client.js) and imported connect fn in play.js using ES6 shorthand syntax

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+
+const connect = function () {
+  // conn object returned by net.createConnection represents the connection with the server
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+
+    console.log(data);
+
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
