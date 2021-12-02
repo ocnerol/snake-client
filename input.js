ee23dbb47e8775015e0a7bba10ac4bb9901f9b9d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-02 03:49:58 +0000

    introduce constants module which stores constant values for IP and PORT, and imported these values in client.js and used them in the function that creates the socket object

--- input.js
+++ input.js
@@ -5,46 +5,46 @@ let connection;
 // setup interface to handle user input from stdin
 
 const setupInput = function(conn) {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
+	connection = conn;
+	const stdin = process.stdin;
+	stdin.setRawMode(true);
+	stdin.setEncoding("utf8");
+	stdin.resume();
 
-  
-  const handleUserInput = (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-    connection.write(`Say: HelloWorld`);
-    switch (key) {
-      case 'w': {
-        connection.write('Move: up');
-        break;
-      }
 
-      case 'a': {
-        connection.write('Move: left');
-        break;
-      }
+	const handleUserInput = (key) => {
+		if (key === '\u0003') {
+			process.exit();
+		}
+		connection.write(`Say: HelloWorld`);
+		switch (key) {
+			case 'w': {
+				connection.write('Move: up');
+				break;
+			}
 
-      case 's': {
-        connection.write('Move: down');
-        break;
-      }
+			case 'a': {
+				connection.write('Move: left');
+				break;
+			}
 
-      case 'd': {
-        connection.write('Move: right');
-        break;
-      }
-    }   
-  };
+			case 's': {
+				connection.write('Move: down');
+				break;
+			}
 
-stdin.on('data', handleUserInput);
+			case 'd': {
+				connection.write('Move: right');
+				break;
+			}
+		}
+	};
 
-return stdin;
+	stdin.on('data', handleUserInput);
+
+	return stdin;
 };
 
 module.exports = {
-  setupInput
-}
+	setupInput
+};
